3b87ea58dd34a416f1690a9b76333b1d9cf1a9e4
Julian-Maccaferri <tulimaccaferri@gmail.com>
2024-10-07 01:07:35 -0300

    Abstraction activity ready

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Programmer";
+        job1._company = "Microsoft";
+        job1._starYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Apple";
+        job2._starYear = 2020;
+        job2._endYear = 2024;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Julian Maccaferri";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
     }
 }
---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,12 @@
+public class Job
+{
+    public string _jobTitle = "";
+    public string _company = "";
+    public int _starYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_starYear}-{_endYear}");
+    }
+}
