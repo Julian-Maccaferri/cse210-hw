6ce5e73ff34485fa974d48d0687cfe9098df6675
Julian-Maccaferri <tulimaccaferri@gmail.com>
2024-10-18 01:05:17 -0300

    Encapsulation Activity Ready

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,55 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction (int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction (int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void SetTopNumber(int top)
+    {
+        _top = top;
+    }
+
+    public int GetTopNumber()
+    {
+        return _top;
+    }
+
+    public void SetBottomNumber(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public int GetBottomNumber()
+    {
+        return _bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return _top + "/" + _bottom;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);    
+
+        Console.WriteLine(fraction1.GetTopNumber());
+        Console.WriteLine(fraction1.GetBottomNumber());
+
+        Console.WriteLine(fraction2.GetTopNumber());
+        Console.WriteLine(fraction2.GetBottomNumber());
+
+        Console.WriteLine(fraction3.GetTopNumber());
+        Console.WriteLine(fraction3.GetBottomNumber());
+
+        Fraction fraction4 = new Fraction();
+        fraction4.SetTopNumber(6);
+        Console.WriteLine(fraction4.GetTopNumber());
+
+        Fraction fraction5 = new Fraction();
+        fraction5.SetTopNumber(3);
+        fraction5.SetBottomNumber(4);
+        Console.WriteLine($"{fraction5.GetTopNumber()}/{fraction5.GetBottomNumber()}");
+        Console.WriteLine(fraction5.GetFractionString());
+        Console.WriteLine(fraction5.GetDecimalValue());
     }
 }
