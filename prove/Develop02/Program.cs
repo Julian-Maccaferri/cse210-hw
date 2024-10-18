081a52faa7df0633136392a4ac79c20b69817435
Julian-Maccaferri <tulimaccaferri@gmail.com>
2024-10-17 23:55:17 -0300

    Start Develop02 program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the journal programm!");
+        Console.WriteLine("Please select one of the following options:")
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Save");
+        Console.WriteLine("4. Load");
+        Console.WriteLine("5. Quit");
+
+        string choiseOption = Console.ReadLine();
+
+        if (choiseOption == "1")
+        {
+            
+        }
+
     }
 }
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    public PromptGenerator()
+    {
+        string prompt1 = "Hola";
+        _prompts.Add(prompt1);
+    }
+}
+
+
+
