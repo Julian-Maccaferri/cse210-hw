7aa91e600b346b2e9761576efd815239aeac23cc
Julian-Maccaferri <tulimaccaferri@gmail.com>
2024-10-29 15:23:13 -0300

    Inheritance Activity Ready

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment a1 = new Assignment("Julian Maccaferri", "Math");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Julian Maccaferri", "Division", "8.5", "9-12");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Bruno Maccaferri", "Poetry", "20 Love Poems");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
